a82c733a4b95cca39b4baaa4e16b4432ab721aac
Wayne Stedman <wstedman@dhccare.com>
2019-04-18 15:45:20 -0700

    desktop draft complete

--- dest/js/astax.js
+++ dest/js/astax.js
@@ -3,10 +3,9 @@
 $(document).ready(function () {
   $('#testimonialsSlider').slick({
     dots: false,
-    // autoplay: true,
+    autoplay: true,
     // autoplaySpeed: 4000,
-    // fade: true,
-    focusOnChange: true,
+    fade: true,
     speed: 1200,
     // cssEase: 'ease-in-out',
     infinite: true,
@@ -16,26 +15,17 @@ $(document).ready(function () {
   }); // testimonials slider
 
   $('#ingredientsDesc').slick({
-    // autoplay: true,
-    // autoplaySpeed: 4000,
     fade: true,
-    // cssEase: 'ease-in-out',
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
     asNavFor: '#ingredientsSlider'
   });
   $('#ingredientsSlider').slick({
-    // autoplay: true,
-    // autoplaySpeed: 4000,
-    // fade: true,
-    // cssEase: 'ease-in-out',
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 1,
     asNavFor: '#ingredientsDesc',
     dots: false,
-    centerMode: true,
-    centerPadding: '60px',
     focusOnSelect: true
   });
 }); //doc ready
--- src/js/astax.js
+++ src/js/astax.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
   $('#testimonialsSlider').slick({
     dots: false,
-    // autoplay: true,
+    autoplay: true,
     // autoplaySpeed: 4000,
-    // fade: true,
-    focusOnChange: true,
+    fade: true,
     speed: 1200,
     // cssEase: 'ease-in-out',
     infinite: true,
@@ -13,26 +12,17 @@ $(document).ready(function(){
     swipe: true
   }); // testimonials slider
   $('#ingredientsDesc').slick({
-    // autoplay: true,
-    // autoplaySpeed: 4000,
     fade: true,
-    // cssEase: 'ease-in-out',
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
     asNavFor: '#ingredientsSlider'
   });
   $('#ingredientsSlider').slick({
-    // autoplay: true,
-    // autoplaySpeed: 4000,
-    // fade: true,
-    // cssEase: 'ease-in-out',
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 1,
     asNavFor: '#ingredientsDesc',
     dots: false,
-    centerMode: true,
-    centerPadding: '60px',
     focusOnSelect: true
   });
 }); //doc ready
